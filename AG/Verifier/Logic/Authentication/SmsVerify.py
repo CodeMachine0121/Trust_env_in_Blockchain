0b52e82cf048d09e40160db8541fbe1547a44eeb
CodeMachine0121m <CodeMachine0121@gmail.com>
2022-06-20 15:33:40 +0800

    OTP 修正

--- AG/Verifier/Logic/Authentication/SmsVerify.py
+++ AG/Verifier/Logic/Authentication/SmsVerify.py
@@ -1,6 +1,6 @@
 import os
 from twilio.rest import Client
-
+from twilio.base.exceptions import TwilioRestException
 
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
@@ -17,10 +17,14 @@ def sendOneTimePassword_SMS(phoneNumber: str):
 
 def verifyOneTimePassword_SMS(phoneNumber: str, otp: str) -> bool:
     client = Client(account_sid, auth_token)
-    verification = client.verify \
-        .services(serviceID) \
-        .verification_checks \
-        .create(to='+886{}'.format(phoneNumber[1:]), code=otp).status
+    try:
+        verification = client.verify \
+            .services(serviceID) \
+            .verification_checks \
+            .create(to='+886{}'.format(phoneNumber[1:]), code=otp).status
+    except TwilioRestException as e:
+        print("[!] TwilioRestException occur")
+        verification = True
     return True if verification == "approved" else False
 
 
